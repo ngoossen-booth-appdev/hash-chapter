5657ab81a30d9e2ca0aa182762cbb170d555b6c9
ngoossen-booth <ngoossen@chicagobooth.edu>
2020-10-08 00:52:18 +0000

    modified:   hash_person.rb

--- hash_person.rb
+++ hash_person.rb
@@ -1,2 +1,9 @@
 # Ask for a name, age, and occupation; store the values in a Hash; display the Hash.
 p "Enter a name, age, and occupation separated by spaces:"
+info = gets.chomp.split
+
+person1 = Hash.new
+person1.store(:name,info[0])
+person1.store(:age,info[1].to_i)
+person1.store(:occupation, info[2])
+p person1
